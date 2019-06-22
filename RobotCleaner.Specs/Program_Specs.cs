fda1975a5a6875fff36f2ec62d80fece97533192
Adam R Moss <adamrmoss@gmail.com>
2019-06-21 21:21:23 -0400

    Main Program

---
+++ RobotCleaner.Specs/Program_Specs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace RobotCleaner.Specs
+{
+    public class ProgramSpecs
+    {
+        [SetUp]
+        public void Setup()
+        {
+            this.stringWriter = new StringWriter();
+        }
+
+        private StringWriter stringWriter;
+
+        [Test]
+        public void Program_SmallCircle()
+        {
+            var input = "4\n" + "-1 -1\n" + "E 2\n" + "N 2\n" + "W 2\n" + "S 2\n";
+            var stringReader = new StringReader(input);
+
+            Program.Main(stringReader, this.stringWriter);
+            var output = this.stringWriter.GetStringBuilder().ToString();
+
+            output.Should().Be("=> Cleaned: 8\r\n");
+        }
+
+        [Test]
+        public void Program_ThereAndBackAgain()
+        {
+            var input = "2\n" + "0 0\n" + "E 20\n" + "W 20\n";
+            var stringReader = new StringReader(input);
+
+            Program.Main(stringReader, this.stringWriter);
+            var output = this.stringWriter.GetStringBuilder().ToString();
+
+            output.Should().Be("=> Cleaned: 21\r\n");
+        }
+    }
+}
--- RobotCleaner/Program.cs
+++ RobotCleaner/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace RobotCleaner
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Main(Console.In, Console.Out);
+        }
+
+        public static void Main(TextReader input, TextWriter output)
+        {
+            var office = new Office();
+            var robot = new Robot(office);
+
+            var numberOfCommands = int.Parse(input.ReadLine());
+
+            var initialPosition = Regex.Split(input.ReadLine(), @"\s+");
+            var initialX = int.Parse(initialPosition[0]);
+            var initialY = int.Parse(initialPosition[1]);
+
+            robot.MoveTo(initialX, initialY);
+
+            for (var i = 0; i < numberOfCommands; i++)
+            {
+                var command = Regex.Split(input.ReadLine(), @"\s+");
+                var direction = Enum.Parse<CardinalDirection>(command[0]);
+                var distance = int.Parse(command[1]);
+
+                robot.Move(direction, distance);
+            }
+
+            output.WriteLine($"=> Cleaned: {office.VisitedLocationsCount}");
         }
     }
 }
